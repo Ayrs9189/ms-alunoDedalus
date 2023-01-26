da423436383d47ccd747529a892feecbb037c2de
Einstein Ayres <ayresasouza@gmail.com>
2023-01-26 14:22:08 -0300

    commit inicial ms aluno dedalus

---
+++ src/test/java/com/msalunodedalus/Dedalus/DedalusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msalunodedalus.Dedalus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DedalusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
